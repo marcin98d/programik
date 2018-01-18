3abd1d8b2425262c04c54ebc07bda25776b4aec8
marcin98d <32922657+marcin98d@users.noreply.github.com>
2018-01-18 12:57:11 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,92 @@
 #include <iostream>
 
 using namespace std;
-struct Point{
-    int x,y;
+
+const int maxCard = 100;
+
+class Set {
+    public:
+        Set() { card = 0; }
+        Set(int tab[], int dlugosc);
+        friend bool operator & (int, const Set&); // membership
+        friend bool operator == (const Set&, const Set&);     // equality
+        friend bool operator != (const Set&, const Set&);     // inequality
+        friend Set operator * (const Set&, const Set&);       // intersection
+        friend Set operator + (const Set&, const Set&);       // union
+        friend ostream& operator << (ostream &os, const Set &set);
+    private:
+        int elems[maxCard];                       // set elements
+        int card;                                 // set cardinality
 };
-struct Node{
-    Point p;
-    Node *next;
-};
-struct List{
-    Node *head;
-    void wyswietl(List list);
-};
-void wyswietl(List list)
-{
-   for(Node *n = list.head; n!=NULL; n=n->next)
-   {
-       cout << n->p.x << endl << n->p.y << endl;
-   }
-}
-void dodaj_pierwszy(List &list, int x, int y)
-{
-    Node *n = new Node();
-    n->p.x=x;
-    n->p.y=y;
-    n->next=list.head;
-    list.head=n;
-}
-void dodaj_ostatni(List &list, int x, int y)
-{
-    for(Node *n=list.head;n!=NULL;n=n->next)
+Set::Set(int tab[], int dlugosc){
+    for(int i=0; i<dlugosc;i++)
     {
-        if(n->next==NULL)
+        elems[i]=tab[i];
+    }
+    card=dlugosc;
+}
+bool operator & (int a, const Set& zbior){
+    for(int i=0;i<zbior.card;i++)
+    {
+        if(zbior.elems[i]==a)
         {
-            Node *tmp = new Node();
-            tmp->p.x=x;
-            tmp->p.y=y;
-            tmp->next=NULL;
-            n->next=tmp;
-            break;
+            return true;
         }
     }
+    return false;
 }
-void insert(Node *node, Point point)
-{
-
+bool operator ==(const Set& zbior, const Set& zbior1){
+    for(int i=0;i<zbior.card;i++)
+    {
+        if(zbior.elems[i]!=zbior1.elems[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+bool operator !=(const Set& zbior, const Set& zbior1){
+    for(int i=0; i<zbior.card;i++)
+    {
+        if(zbior1.elems[i] == zbior1.elems[i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+Set operator *(const Set& zbior, const Set& zbior1){
+    int dlugosc = 0;
+    int tab[maxCard];
+    for(int i=0;i<zbior.card;i++)
+    {
+        for(int j=0;j<zbior1.card;j++)
+        {
+            if(zbior.elems[i]==zbior1.elems[i])
+            {
+                tab[dlugosc]=zbior.elems[i];
+                dlugosc++;
+                break;
+            }
+        }
+    }
+    Set hwdp(tab, dlugosc);
+    return hwdp;
+}
+ostream& operator << (ostream &os, const Set &set){
+    for(int i=0;i<set.card;i++)
+    {
+        os << set.elems[i] << " ";
+    }
+    return os;
 }
 int main()
 {
-    List lamana;
-    lamana.head = NULL;
-    dodaj_pierwszy(lamana,25,30);
-    dodaj_ostatni(lamana,50,50);
-    dodaj_pierwszy(lamana,10,20);
-
-    wyswietl(lamana);
+    Set s;
+    int tab[]={1,2,3,4,5};
+    int tab1[]={5,6,7,8,9};
+    Set jp(tab,5);
+    Set jd(tab1,5);
+    cout << (jp * jd);
     return 0;
 }
